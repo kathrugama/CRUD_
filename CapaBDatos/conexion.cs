7732e18bd0e591efdbad1044ca6c9bf06d970a6a
Katherine Ruiz <kruiz780@gmail.com>
2022-08-25 11:33:33 -0600

    Agregar archivos de proyecto.

---
+++ CapaBDatos/conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaBDatos
+{
+    public class conexion
+    {
+     
+        public SqlConnection Con = new SqlConnection("Data Source=DESKTOP-K1IOKQ5\\Katherine Rugama; Initial Catalog=Computadora;Integrated Security=True");
+            public SqlConnection AbrirConexion()
+            {
+                if (Con.State == ConnectionState.Closed)
+                    Con.Open();
+                return Con;
+            }
+            public SqlConnection CerrarConexion()
+            {
+                if (Con.State == ConnectionState.Open)
+                    Con.Close();
+                return Con;
+            }
+        
+    }
+}
---
+++ CapaPresentación/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace CapaPresentación
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.CmbTipo_de_Computadora = new System.Windows.Forms.TextBox();
+            this.txtProcesador = new System.Windows.Forms.TextBox();
+            this.txtMemoria_RAM = new System.Windows.Forms.TextBox();
+            this.txtTipo_de_disco_duro = new System.Windows.Forms.TextBox();
+            this.txtAlmacenamiento = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(157, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CÓDIGO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(157, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TIPO DE COMPUTADORA:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(157, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "PROCESADOR:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(157, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "MEMORIA RAM:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(157, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(174, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "TIPO DE DISCO DURO:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(157, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "ALMACENAMIENTO:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(317, 39);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(307, 22);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "DETALLES DE UNA COMPUTADORA";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(382, 131);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(384, 20);
+            this.txtCodigo.TabIndex = 7;
+            // 
+            // CmbTipo_de_Computadora
+            // 
+            this.CmbTipo_de_Computadora.Location = new System.Drawing.Point(382, 173);
+            this.CmbTipo_de_Computadora.Name = "CmbTipo_de_Computadora";
+            this.CmbTipo_de_Computadora.Size = new System.Drawing.Size(384, 20);
+            this.CmbTipo_de_Computadora.TabIndex = 8;
+            // 
+            // txtProcesador
+            // 
+            this.txtProcesador.Location = new System.Drawing.Point(382, 220);
+            this.txtProcesador.Name = "txtProcesador";
+            this.txtProcesador.Size = new System.Drawing.Size(384, 20);
+            this.txtProcesador.TabIndex = 9;
+            // 
+            // txtMemoria_RAM
+            // 
+            this.txtMemoria_RAM.Location = new System.Drawing.Point(382, 267);
+            this.txtMemoria_RAM.Name = "txtMemoria_RAM";
+            this.txtMemoria_RAM.Size = new System.Drawing.Size(384, 20);
+            this.txtMemoria_RAM.TabIndex = 10;
+            // 
+            // txtTipo_de_disco_duro
+            // 
+            this.txtTipo_de_disco_duro.Location = new System.Drawing.Point(382, 314);
+            this.txtTipo_de_disco_duro.Name = "txtTipo_de_disco_duro";
+            this.txtTipo_de_disco_duro.Size = new System.Drawing.Size(384, 20);
+            this.txtTipo_de_disco_duro.TabIndex = 11;
+            // 
+            // txtAlmacenamiento
+            // 
+            this.txtAlmacenamiento.Location = new System.Drawing.Point(382, 363);
+            this.txtAlmacenamiento.Name = "txtAlmacenamiento";
+            this.txtAlmacenamiento.Size = new System.Drawing.Size(384, 20);
+            this.txtAlmacenamiento.TabIndex = 12;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsertar.Location = new System.Drawing.Point(160, 434);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(96, 23);
+            this.btnInsertar.TabIndex = 13;
+            this.btnInsertar.Text = "INSERTAR";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(331, 434);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(95, 23);
+            this.btnEliminar.TabIndex = 14;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(513, 434);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(98, 23);
+            this.btnModificar.TabIndex = 15;
+            this.btnModificar.Text = "MODIFICAR";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(683, 434);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(83, 23);
+            this.btnBuscar.TabIndex = 16;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 510);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.txtAlmacenamiento);
+            this.Controls.Add(this.txtTipo_de_disco_duro);
+            this.Controls.Add(this.txtMemoria_RAM);
+            this.Controls.Add(this.txtProcesador);
+            this.Controls.Add(this.CmbTipo_de_Computadora);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox CmbTipo_de_Computadora;
+        private System.Windows.Forms.TextBox txtProcesador;
+        private System.Windows.Forms.TextBox txtMemoria_RAM;
+        private System.Windows.Forms.TextBox txtTipo_de_disco_duro;
+        private System.Windows.Forms.TextBox txtAlmacenamiento;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
